5c327e8138dac4feb1a1f47001ec976c26abe8ab
biru60 <u1904060@student.cuet.ac.bd>
2023-10-28 04:42:21 +0600

    individual and all data can be collected from database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config()
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.fjhyf9f.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,12 +25,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const services=client.db("car-doctor").collection("services");
+
+
+    app.get('/services',async(req,res)=>{
+        const result=await services.find().toArray()
+        res.send(result)
+    })
+
+    app.get("/services/checkout/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await services.findOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
